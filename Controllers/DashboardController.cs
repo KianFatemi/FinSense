60391a49a7402fbbbef2908e358385aad3b308c1
KianFatemi <kianfatemi044@gmail.com>
2025-06-30 14:15:35 -0400

    added new controller and endpoint for dashboard

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinanceDashboard.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceDashboard.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        
+        public DashboardController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetSpendingByCategort()
+        {
+            var currentId =  _userManager.GetUserId(User);
+
+            var spendingData = await _context.Transactions
+                .Where(t => t.FinancialAccount.UserID == currentId)
+                .Where(t => t.Amount < 0)
+                .GroupBy(t => t.Category)
+                .Select(group => new
+                {
+                    Category = group.Key,
+                    TotalAmount = group.Sum(t => Math.Abs(t.Amount)),
+                })
+                .OrderBy(s => s.TotalAmount)
+                .ToListAsync();
+            return Ok(spendingData);
+        }
+    }
+}
